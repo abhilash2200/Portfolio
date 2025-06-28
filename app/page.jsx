a29762be2a6b17428a87f2accb7e979f58920e2c
abhilash2616 <abhilashbera2003@gmail.com>
2025-06-28 22:47:24 +0530

    .

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,35 @@
-export default function Home() {
+"use client"
+import { FiDownload } from "react-icons/fi"
+import { Button } from "@/components/ui/button"
+import Social from "@/components/Social"
+
+const Home = () => {
   return (
-    <div className="container mx-auto px-4">
-      <h1 className="font-jetbrains text-2xl">homepage</h1>
-    </div>
+    <section className="h-full">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col md:flex-row items-center justify-between">
+          <div>
+            <span>Frontend Developer</span>
+            <h1 className="text-4xl font-bold">Hello I'm <br/><span className="text-blue-500">Abhilash Bera</span></h1>
+            <p className="max-w-[400px] mb-9 text-gray-500">I'm a web developer with a passion for creating beautiful and functional websites.</p>
+            <div className="flex flex-wrap mf:flex-col items-center gap-8">
+              <Button className="flex items-center gap-2" variant="outline" size="lg">
+                <span>Download CV</span>
+                <FiDownload className="ml-2" />
+              </Button>
+              <div className="mb-8 lg:mb-0">
+                <Social containerStyles="flex items-center gap-4" iconStyles="w-10 h-10 text-gray-500 hover:text-blue-500 transition-colors duration-300" />
+              </div>
+            </div>
+          </div>
+          <div>
+            photo
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
+
+export default Home
+
